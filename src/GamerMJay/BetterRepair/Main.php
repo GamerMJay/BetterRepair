79a2b56f9aa0a3d4075df2653c49722db2fc88cc
GamerMJay <66413999+GamerMJay@users.noreply.github.com>
2022-01-29 23:25:12 +0100

    Update Main.php

--- src/GamerMJay/BetterRepair/Main.php
+++ src/GamerMJay/BetterRepair/Main.php
@@ -1,1 +1,24 @@
-.
+<?php
+
+namespace GamerMJay\BetterGameMode;
+
+#pocketmine
+use GamerMJay\BetterRepair\commands\repair;
+use pocketmine\command\Command;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\tile\Tile;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use pocketmine\Server;
+use pocketmine\player\Player;
+
+class Main extends PluginBase {
+    public $config;
+
+    public function onEnable(): void {
+        $this->saveResource("config.yml");
+        $this->config = new Config($this->getDataFolder() . "config.yml", 2);
+        $this->getServer()->getCommandMap()->register("repair", new repair($this));
+    }
+}
